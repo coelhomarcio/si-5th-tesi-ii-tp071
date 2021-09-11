eed482df8ceef13896d13e4c4e75d3b45f329376
Marcio Coelho <marciocoelho@gmail.com>
2021-09-11 20:57:05 -0300

    Created home.component.*

---
+++ src/app/component/home/home.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from "@angular/core";
+
+import { DisciplinesService } from "../../service/disciplines.service";
+
+@Component({
+  selector:    "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls:   ["./home.component.scss"]
+})
+export class HomeComponent implements OnInit {
+  subTitle = "Home";
+  name = "Marcio Rodrigues Paiva Coelho";
+  ra = "0050831921015";
+  gits = [
+    { label: "GitHub Page", link: "https://coelhomarcio.github.io" },
+    { label: "Repositório GitHub", link: "https://github.com/coelhomarcio" }
+  ];
+
+  constructor(public discipline: DisciplinesService) {
+  }
+
+  ngOnInit(): void {
+  }
+}
