fdd7a00071b77fb201095c119907aefed6e7ae44
Marcio Coelho <marciocoelho@gmail.com>
2021-09-11 20:53:26 -0300

    Updated app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule }             from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
+import { HomeComponent }        from "./component/home/home.component";
+import { DisciplinesComponent } from "./component/disciplines/disciplines.component";
+import { GradesComponent }      from "./component/grades/grades.component";
+
 const routes: Routes = [
-  { path: "", redirectTo: "home", pathMatch: "full" }
+  { path: "", redirectTo: "home", pathMatch: "full" },
+  { path: "home", component: HomeComponent },
+  { path: "disciplines", component: DisciplinesComponent },
+  { path: "grades", component: GradesComponent }
 ];
 
 @NgModule({
