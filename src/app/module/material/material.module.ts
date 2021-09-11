4241d6e61b9d5db9b2749bdfb3b38069cafa2b30
Marcio Coelho <marciocoelho@gmail.com>
2021-09-11 20:53:55 -0300

    Updated material.module.ts

--- src/app/module/material/material.module.ts
+++ src/app/module/material/material.module.ts
@@ -1,6 +1,17 @@
-import { NgModule } from "@angular/core";
+import { NgModule }         from "@angular/core";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatButtonModule }  from "@angular/material/button";
+import { MatIconModule }    from "@angular/material/icon";
+import { MatMenuModule }    from "@angular/material/menu";
+import { MatListModule }    from "@angular/material/list";
 
-const materials = [];
+const materials = [
+  MatToolbarModule,
+  MatButtonModule,
+  MatIconModule,
+  MatMenuModule,
+  MatListModule
+];
 
 @NgModule({
   imports: materials,
