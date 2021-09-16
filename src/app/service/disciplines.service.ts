5f7a3e3bf75d1b5be6a582a59d184271ee2fef28
Marcio Coelho <marciocoelho@gmail.com>
2021-09-16 18:15:37 -0300

    Updated disciplines.service.ts

--- src/app/service/disciplines.service.ts
+++ src/app/service/disciplines.service.ts
@@ -1,55 +1,38 @@
 import { Injectable } from "@angular/core";
 
-interface Discipline {
-  weekday: string,
-  time: string,
-  discipline: string,
-  p1: number,
-  p2: number
+export interface Discipline {
+  id: number | null,
+  discipline: string | null,
+  weekday: string | null,
+  time: string | null,
+  p1: number | null,
+  p2: number | null
 }
 
 const DISCIPLINES: Discipline[] = [
   {
-    weekday:    "Segunda-feira",
-    time:       "19:00",
-    discipline: "Desenvolvimento para Dispositivos Móveis I",
-    p1:         NaN,
-    p2:         NaN
+    id:      1, discipline: "Desenvolvimento para Dispositivos Móveis I",
+    weekday: "Segunda-feira", time: "19:00", p1: null, p2: null
   },
   {
-    weekday:    "Quarta-feira",
-    time:       "19:00",
-    discipline: "Negócios e Marketing e Eletrônicos",
-    p1:         NaN,
-    p2:         NaN
+    id:      2, discipline: "Negócios e Marketing e Eletrônicos",
+    weekday: "Quarta-feira", time: "19:00", p1: null, p2: null
   },
   {
-    weekday:    "Quinta-feira",
-    time:       "19:00",
-    discipline: "Desenvolvimento para Servidores II",
-    p1:         NaN,
-    p2:         NaN
+    id:      3, discipline: "Desenvolvimento para Servidores II",
+    weekday: "Quinta-feira", time: "19:00", p1: null, p2: null
   },
   {
-    weekday:    "Sexta-feira",
-    time:       "19:00",
-    discipline: "Tópicos Especiais em Sistemas para Internet II",
-    p1:         NaN,
-    p2:         NaN
+    id:      4, discipline: "Tópicos Especiais em Sistemas para Internet II",
+    weekday: "Sexta-feira", time: "19:00", p1: null, p2: null
   },
   {
-    weekday:    "Sábado",
-    time:       "08:00",
-    discipline: "Projeto de Prototipagem e Testes de Usabilidade",
-    p1:         NaN,
-    p2:         NaN
+    id:      5, discipline: "Projeto de Prototipagem e Testes de Usabilidade",
+    weekday: "Sábado", time: "08:00", p1: null, p2: null
   },
   {
-    weekday:    "Sábado",
-    time:       "11:30",
-    discipline: "Projeto de Trabalho de Graduação em Sistemas para Internet I",
-    p1:         NaN,
-    p2:         NaN
+    id:      6, discipline: "Projeto de Trabalho de Graduação em Sistemas para Internet I",
+    weekday: "Sábado", time: "11:30", p1: null, p2: null
   }
 ];
 
@@ -58,16 +41,73 @@ const DISCIPLINES: Discipline[] = [
 })
 export class DisciplinesService {
   private _disciplines: Discipline[];
+  private _discipline: Discipline;
 
   constructor() {
-    this._disciplines = DISCIPLINES;
+    this._disciplines = Object.assign([], DisciplinesService.getLocalStorage());
+    this._discipline = {
+      id: null, discipline: null, weekday: null, time: null, p1: null, p2: null
+    };
   }
 
   public get disciplines(): Discipline[] {
     return this._disciplines;
   }
 
-  public set disciplines(value: Discipline[]) {
+  private set disciplines(value: Discipline[]) {
     this._disciplines = value;
+  }
+
+  public get discipline(): Discipline {
+    return this._discipline;
+  }
+
+  public set discipline(value: Discipline) {
+    this._discipline = value;
+  }
+
+  private static getLocalStorage(): Discipline[] {
+    if (localStorage.hasOwnProperty("disciplines"))
+      return JSON.parse(localStorage.disciplines);
+    else
+      return JSON.parse(DisciplinesService.setLocalStorage(DISCIPLINES));
+  }
+
+  private static setLocalStorage(disciplineArr: Discipline[]) {
+    return localStorage.disciplines = JSON.stringify(disciplineArr);
+  }
+
+  public saveDiscipline() {
+    if (this.disciplines !== DisciplinesService.getLocalStorage())
+      this.disciplines = Object.assign([], DisciplinesService.getLocalStorage());
+    if (this.discipline.id === null) {
+      if (this.disciplines.length) {
+        const maxId = this.disciplines.reduce((el1, el2) => {
+          return el1.id! > el2.id! ? el1 : el2;
+        }).id!;
+        this.discipline.id = maxId + 1;
+      }
+      else
+        this.discipline.id = 1;
+      this.disciplines.push(this.discipline);
+    }
+    else {
+      const disciplineIndex = this.disciplines.findIndex(el => el.id === this.discipline.id);
+      if (disciplineIndex === -1)
+        this.disciplines.push(this.discipline);
+      else
+        this.disciplines[disciplineIndex] = Object.assign({}, this.discipline);
+    }
+    DisciplinesService.setLocalStorage(this.disciplines);
+  }
+
+  public deleteDiscipline(id: number) {
+    if (this.disciplines !== DisciplinesService.getLocalStorage())
+      this.disciplines = Object.assign([], DisciplinesService.getLocalStorage());
+    const disciplineIndex = this.disciplines.findIndex(el => el.id === id);
+    if (disciplineIndex !== -1) {
+      this.disciplines.splice(disciplineIndex, 1);
+      DisciplinesService.setLocalStorage(this.disciplines);
+    }
   }
 }
