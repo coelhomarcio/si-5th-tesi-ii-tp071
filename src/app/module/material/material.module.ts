ad37e549c335aedd449b268f4386daf754e72ed7
Marcio Coelho <marciocoelho@gmail.com>
2021-09-16 18:10:04 -0300

    Updated material.module.ts

--- src/app/module/material/material.module.ts
+++ src/app/module/material/material.module.ts
@@ -1,16 +1,24 @@
-import { NgModule }         from "@angular/core";
-import { MatToolbarModule } from "@angular/material/toolbar";
-import { MatButtonModule }  from "@angular/material/button";
-import { MatIconModule }    from "@angular/material/icon";
-import { MatMenuModule }    from "@angular/material/menu";
-import { MatListModule }    from "@angular/material/list";
+import { NgModule }           from "@angular/core";
+import { MatToolbarModule }   from "@angular/material/toolbar";
+import { MatButtonModule }    from "@angular/material/button";
+import { MatIconModule }      from "@angular/material/icon";
+import { MatMenuModule }      from "@angular/material/menu";
+import { MatListModule }      from "@angular/material/list";
+import { MatTooltipModule }   from "@angular/material/tooltip";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule }     from "@angular/material/input";
+import { MatSelectModule }    from "@angular/material/select";
 
 const materials = [
   MatToolbarModule,
   MatButtonModule,
   MatIconModule,
   MatMenuModule,
-  MatListModule
+  MatListModule,
+  MatTooltipModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatSelectModule
 ];
 
 @NgModule({
